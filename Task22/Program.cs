053a3fb7dcb1f8be4d3d0ab909682b0af41094f5
Vyacheslav Nosov <slavan133@yandex.ru>
2023-05-31 00:30:05 +0300

    чзх

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N;
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (N <= 0)
+{
+ Console.WriteLine("Некорректно заданы данные");   
+}
+else
+{
+while ( i < N );{
+    int j = i * i;
+    Console.WriteLine(j);
+    i++;
+    }
+}
