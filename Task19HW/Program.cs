3287129df4543eb469045cd9ed4811ed38f6a90c
Vyacheslav Nosov <slavan133@yandex.ru>
2023-05-31 23:07:53 +0300

    Task19HW done

---
+++ Task19HW/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int length = number.Length;
+if (length == 5)
+{
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine("число является полиномом");
+}
+else Console.WriteLine("число не является полиномом");
+}
+else Console.WriteLine("число не пятизначное");
